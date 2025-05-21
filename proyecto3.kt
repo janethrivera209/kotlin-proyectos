5889135fabff508a63e8c1835f66de959ab0d1bb
janethrivera209 <riverarobledojaneth@gmail.com>
2025-05-20 19:03:57 -0600

    Add files via upload

---
+++ proyecto3.kt
@@ -0,0 +1,6 @@
+fun main(argumento: Array<String>){
+    val lado = 50
+    val perimetro = lado * 4
+    val superficie = lado * lado
+    print("El perímetro de un cuadrado de lado $lado es $perimetro y su superficie es $superficie")
+}
