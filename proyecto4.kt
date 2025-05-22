595f661e5e99165917e69701f50c6deafbf56c7e
janethrivera209 <riverarobledojaneth@gmail.com>
2025-05-21 18:11:26 -0600

    Add files via upload

---
+++ proyecto4.kt
@@ -0,0 +1,7 @@
+fun main(argumento: Array<String>) {
+    val peso1 = 89.4f
+    val peso2 = 67f
+    val peso3 = 87.45f
+    val promedio = (peso1 + peso2 + peso3) / 3
+    println("El promedio de los tres pesos de personas es $promedio")
+}
