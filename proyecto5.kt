859947d8bbb79ba9cbf071f356573d7298435c0c
janethrivera209 <riverarobledojaneth@gmail.com>
2025-05-22 16:24:32 -0600

    Add files via upload

---
+++ proyecto5.kt
@@ -0,0 +1,10 @@
+fun main(argumento: Array<String>) {
+    print("Ingrese primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readln().toInt()
+    val suma = valor1 + valor2
+    println("La suma de $valor1 y $valor2 es $suma")
+    val producto = valor1 * valor2
+    println("El producto de $valor1 y $valor2 es $producto")
+}
