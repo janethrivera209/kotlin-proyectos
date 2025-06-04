91754313d9bdc1164d1b6a464c0826fbf6f967c8
janethrivera209 <riverarobledojaneth@gmail.com>
2025-06-03 18:57:14 -0600

    Add files via upload

---
+++ proyecto15.kt
@@ -0,0 +1,8 @@
+fun main(parametro: Array<String>) {
+    print("Ingrese primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readln().toInt()
+    val mayor = if (valor1 > valor2) valor1 else valor2
+    println("El mayor entre $valor1 y $valor2 es $mayor")
+}
