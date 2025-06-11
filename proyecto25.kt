73e7b2596b6a73683575b2ebb4afec6248c77234
janethrivera209 <riverarobledojaneth@gmail.com>
2025-06-11 17:16:27 -0600

    Add files via upload

---
+++ proyecto25.kt
@@ -0,0 +1,15 @@
+fun main(parametro: Array<String>) {
+    print("Ingrese primer valor:")
+    val num1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val num2 = readln().toInt()
+    print("Ingrese tercer valor:")
+    val num3 = readln().toInt()
+    if (num1 > num2 && num1 > num3)
+        print(num1)
+    else
+        if (num2 > num3)
+            print(num2)
+        else
+            print(num3);
+}
