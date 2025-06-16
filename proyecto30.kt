dee435c52fce0156832eebb3aea77bce041eb94d
janethrivera209 <riverarobledojaneth@gmail.com>
2025-06-16 16:31:09 -0600

    Add files via upload

---
+++ proyecto30.kt
@@ -0,0 +1,10 @@
+fun main(parametro: Array<String>) {
+    print("Ingrese primer valor:")
+    val valor1 = readln().toInt()
+    print("Ingrese segundo valor:")
+    val valor2 = readln().toInt()
+    print("Ingrese tercer valor:")
+    val valor3 = readln().toInt()
+    if (valor1 < 10 || valor2 < 10 || valor3 < 10)
+        print("Alguno de los números es menor a diez")
+}
