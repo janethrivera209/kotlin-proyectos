fee723be31f17dbe9771e6e08f35836ed81e1f53
janethrivera209 <riverarobledojaneth@gmail.com>
2025-08-13 09:07:12 -0600

    Add files via upload

---
+++ proyecto38.kt
@@ -0,0 +1,14 @@
+fun main(parametro: Array<String>) {
+    print("Cuantas alturas ingresará?:")
+val n = readln().toInt()
+var x = 1
+var suma = 0.0
+while (x <= n) {
+    print("Ingrese la altura de la persona(Ej:1.76) :")
+    val altura = readln().toDouble()
+    suma = suma + altura
+    x = x + 1
+}
+val promedio = suma / n
+println("Altura promedio: $promedio")
+}
