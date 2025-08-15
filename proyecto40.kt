e19c6141d534a0e366e0941106e9438af3cda918
janethrivera209 <riverarobledojaneth@gmail.com>
2025-08-15 10:35:35 -0600

    Add files via upload

---
+++ proyecto40.kt
@@ -0,0 +1,9 @@
+fun main(parametro: Array<String>) {
+    var termino = 11
+    var x = 1
+    while (x <= 25) {
+        println(termino)
+        termino = termino + 11
+        x = x + 1
+    }
+}
